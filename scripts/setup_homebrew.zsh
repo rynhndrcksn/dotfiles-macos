8d1e927b78473194766a3dfc95721c5ffb35cd73
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2022-08-28 16:50:37 -0700

    Created setup_homebrew.zsh
    Installs Homebrew and software/applications.

---
+++ scripts/setup_homebrew.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+# Author: Ryan H.
+# Last Updated: August 28, 2022.
+# Filename: setup_homebrew.zsh
+# Installs Homebrew and installs wanted software/applications.
+
+echo ""
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# CLI tools/software
+brew install ansible
+brew install bat
+brew install go
+brew install htop
+brew install php7.4
+brew install tldr
+
+# Applications
+brew install --cask firefox
+brew install --cask vscodium
+brew install --cask vagrant
