eea563f029eb2866513311e3ead561f6809d6d11
Ryan H <4751093-rynhndrcksn@users.noreply.gitlab.com>
2022-09-05 14:04:35 -0700

    Added Brewfile
    Changed setup_homebrew.zsh to use Brewfile.

---
+++ Brewfile
@@ -0,0 +1,19 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews (packages)
+brew "ansible"
+brew "bat"
+brew "go"
+brew "htop"
+brew "php@7.4"
+brew "tldr"
+
+# Casks (applications)
+cask "alfred"
+cask "firefox"
+cask "lando"
+cask "vagrant"
+cask "vscodium"
--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -1,27 +1,12 @@
 #!/usr/bin/env zsh
 # Author: Ryan H.
 # Filename: setup_homebrew.zsh
-# Installs Homebrew and installs wanted software/applications.
+# Installs Homebrew and installs wanted software.
 
 echo "\n<<< Homebrew installation starting >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# CLI tools/software
-brew install ansible
-brew install bat
-brew install docker
-brew install docker-compose
-brew install go
-brew install htop
-brew install php@7.4
-brew install tldr
-
-# Applications
-brew install --cask --no-quarantine alfred
-#brew install --cask --no-quarantine firefox
-brew install --cask --no-quarantine lando
-brew install --cask --no-quarantine vscodium
-brew install --cask --no-quarantine vagrant
+brew bundle --verbose
 
 echo "\n<<< Homebrew installation finished >>>\n"
