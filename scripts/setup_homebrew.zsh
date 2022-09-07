a2075417993c4037041e81488d73b64438d1b3f3
Ryan Hendrickson <4751093-rynhndrcksn@users.noreply.gitlab.com>
2022-09-06 19:50:05 -0700

    Exists function now prevents extra Homebrew installations.

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -3,10 +3,12 @@
 # Filename: setup_homebrew.zsh
 # Installs Homebrew and installs wanted software.
 
-echo "\n<<< Homebrew installation starting >>>\n"
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+    echo "\n<<< Homebrew installed already, skipping installation >>>\n"
+else
+    echo "\n<<< Homebrew installation starting >>>\n"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "\n<<< Homebrew installation finished >>>\n"
+fi
 
 brew bundle --verbose
-
-echo "\n<<< Homebrew installation finished >>>\n"
