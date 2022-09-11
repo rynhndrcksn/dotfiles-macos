24d507cb21561a792f2ffe84596192b34343888a
Ryan Hendrickson <4751093-rynhndrcksn@users.noreply.gitlab.com>
2022-09-11 14:06:25 -0700

    Made setup_zsh.zsh work on M1 macs instead of intel ones.

--- scripts/setup_zsh.zsh
+++ scripts/setup_zsh.zsh
@@ -5,20 +5,20 @@
 
 echo "\n<<< Starting ZSH setup >>>\n"
 
-# Add /usr/local/bin/zsh to /etc/shells
-if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
-    echo '/usr/local/bin/zsh already exists in /etc/shells'
+# Add /opt/homebrew/bin/zsh to /etc/shells
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+    echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
 else
     echo "Enter superuser (sudo) password to edit /etc/shells"
-    echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+    echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
-# Change default shell from /bin/zsh to /usr/local/bin/zsh
-if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
-    echo '$SHELL is already /usr/local/bin/zsh'
+# Change default shell from /bin/zsh to /opt/homebrew/bin/zsh
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+    echo '$SHELL is already /opt/homebrew/bin/zsh'
 else
     echo "Enter user password to change login shell"
-    chsh -s '/usr/local/bin/zsh'
+    chsh -s '/opt/homebrew/bin/zsh'
     echo "Success! Logout and back in for changes to take effect."
 
 fi
@@ -29,5 +29,5 @@ if sh --version | grep -q zsh; then
     echo '/private/var/select/sh already linked to /bin/zsh'
 else
     echo "Enter superuser (sudo) password to symlink sh to zsh"
-    # sudo ln -sfv /bin/zsh /private/var/select/sh
+    sudo ln -sfv /bin/zsh /private/var/select/sh
 fi
