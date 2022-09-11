48177b448fd4cc2b86724fd2c5c2c2e5b8c6fb25
Ryan Hendrickson <4751093-rynhndrcksn@users.noreply.gitlab.com>
2022-09-10 17:51:49 -0700

    Added conditional logic to setup_zsh.zsh

--- scripts/setup_zsh.zsh
+++ scripts/setup_zsh.zsh
@@ -5,9 +5,29 @@
 
 echo "\n<<< Starting ZSH setup >>>\n"
 
-echo "Enter superuser (sudo) password to add Homebrew's ZSH to /etc/shells"
-echo "This makes Homebrew's ZSH program an approved shell for MacOS to use."
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# Add /usr/local/bin/zsh to /etc/shells
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+    echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+    echo "Enter superuser (sudo) password to edit /etc/shells"
+    echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-chsh -s '/usr/local/bin/zsh'
-echo "Success! Logout and back in for changes to take effect."
+# Change default shell from /bin/zsh to /usr/local/bin/zsh
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+    echo '$SHELL is already /usr/local/bin/zsh'
+else
+    echo "Enter user password to change login shell"
+    chsh -s '/usr/local/bin/zsh'
+    echo "Success! Logout and back in for changes to take effect."
+
+fi
+
+# Change sh to be aliased to MacOS's ZSH install rather than BASH.
+# Note: Using /usr/local/bin/zsh doesn't work well, so we have to use MacOS's ZSH.
+if sh --version | grep -q zsh; then
+    echo '/private/var/select/sh already linked to /bin/zsh'
+else
+    echo "Enter superuser (sudo) password to symlink sh to zsh"
+    # sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
