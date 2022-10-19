26b55232b5b2230924a764e705106f4535f5eaf9
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2022-10-18 21:54:28 -0700

    Fixed setup_homebrew.zsh
    Asks for sudo password up front so script doesn't break.
    Allows install to set host name.
    Check if Rosetta 2 was installed already or not.
    Fixed vscode extension install, so it actually installs the extensions.

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -3,6 +3,20 @@
 # Filename: setup_homebrew.zsh
 # Installs Homebrew and installs wanted software.
 
+echo "\n<<< Ask for sudo password up-front >>>\n"
+sudo -v
+
+# Change laptop hostname to 
+vared -p 'What would you like the hostname to be? ' -c hostName
+sudo scutil --set HostName $hostName
+
+if /usr/bin/pgrep oahd >/dev/null 2>&1; then
+    echo "\n<<< Rosetta 2 installed already, skipping installation >>>\n"
+else
+    echo "\n<<< Rosetta 2 installation starting >>>\n"
+    sudo softwareupdate --install-rosetta
+fi
+
 echo "\n<<< Starting Homebrew setup >>>\n"
 
 if exists brew; then
@@ -13,7 +27,11 @@ else
     echo "\n<<< Homebrew installation finished >>>\n"
 fi
 
+echo "\n<<< Brewfile installation starting >>>\n"
 brew bundle --verbose
 
 echo "\n<<< Installing VS Code Extensions >>>\n"
-cat ../config/vscode_extensions | xargs -L 1 code --install-extension
+cat ../config/vscode_extensions | while read extension || [[ -n $extension ]];
+do
+  codium --install-extension $extension --force
+done
