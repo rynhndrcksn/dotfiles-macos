3729932b3392fa4445c32f9fc10d06cfebb1f1eb
Ryan Hendrickson <8751750+TahomaTech@users.noreply.github.com>
2023-04-11 19:58:19 -0700

    Remove global NPM installs

--- scripts/setup_node.zsh
+++ scripts/setup_node.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # Author: Ryan H.
 # Filename: setup_node.zsh
-# Installs 'n' and Node.
+# Installs 'n' and Node LTS.
 
 echo "\n<<< Starting Node setup >>>\n"
 
@@ -9,12 +9,5 @@ if exists $N_PREFIX/bin/node; then
   echo "\n<<< Node $($N_PREFIX/bin/node --version) & NPM $($N_PREFIX/bin/npm --version) already installed with n >>>\n"
 else
   echo "\n<<< Installing Node & NPM with n... >>>\n"
-  n latest
+  n lts
 fi
-
-echo "\n<<< Global NPM installs starting >>>\n"
-# Global NPM installs
-npm i -g typescript
-
-echo "\n<<< Global NPM installs finished >>>\n"
-npm list --global --depth=0 
