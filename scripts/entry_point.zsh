81a184a303c0dcf2040a7f118ca9d9fda42f8ec7
Ryan Hendrickson <8751750+TahomaTech@users.noreply.github.com>
2023-09-03 13:57:13 -0700

    Add sketchybar + plugins

---
+++ scripts/entry_point.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+# Author: Ryan H.
+# Filename: entry_point.zsh
+# Entry point for setting up script files.
+
+# ./setup_homebrew.zsh;
+# ./setup_macos.zsh;
+# ./setup_node.zsh;
+# ./setup_zsh.zsh;
