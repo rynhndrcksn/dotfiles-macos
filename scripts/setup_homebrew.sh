49df9f7135f1bc84b6d422f632b500653dbe84cc
Ryan Hendrickson <8751750+TahomaTech@users.noreply.github.com>
2023-10-11 21:28:31 -0700

    Fix bash warnings in setup_homebrew.sh

--- scripts/setup_homebrew.sh
+++ scripts/setup_homebrew.sh
@@ -3,38 +3,39 @@
 # Filename: setup_homebrew.sh
 # Installs Homebrew and installs wanted software.
 
-echo "\n<<< Ask for sudo password up-front >>>\n"
+printf '\n<<< Ask for sudo password up-front >>>\n'
 sudo -v
 
 # Change laptop hostname to 
-vared -p 'What would you like the hostname to be? ' -c hostName
-sudo scutil --set HostName $hostName
+printf 'What would you like the hostname to be? '
+read -r hostname
+sudo "scutil --set HostName $hostname"
 
 if /usr/bin/pgrep oahd >/dev/null 2>&1; then
-    echo "\n<<< Rosetta 2 installed already, skipping installation >>>\n"
+    printf '\n<<< Rosetta 2 installed already, skipping installation >>>\n'
 else
-    echo "\n<<< Rosetta 2 installation starting >>>\n"
+    printf '\n<<< Rosetta 2 installation starting >>>\n'
     sudo softwareupdate --install-rosetta
 fi
 
-echo "\n<<< Starting Homebrew setup >>>\n"
+printf '\n<<< Starting Homebrew setup >>>\n'
 
 if exists brew; then
-    echo "\n<<< Homebrew installed already, skipping installation >>>\n"
+    printf '\n<<< Homebrew installed already, skipping installation >>>\n'
 else
-    echo "\n<<< Homebrew installation starting >>>\n"
+    printf '\n<<< Homebrew installation starting >>>\n'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "\n<<< Homebrew installation finished >>>\n"
+    printf '\n<<< Homebrew installation finished >>>\n'
 fi
 
-echo "\n<<< Brewfile installation starting >>>\n"
+printf '\n<<< Brewfile installation starting >>>\n'
 brew bundle --verbose
 
 # Rebuild 'bat' cache so it finds custom themes
 bat cache --build
 
-echo "\n<<< Installing VS Code Extensions >>>\n"
-cat ./config/vscode_extensions | while read extension || [[ -n $extension ]];
-do
-  codium --install-extension $extension --force
-done
+# printf '\n<<< Installing VS Code Extensions >>>\n'
+# cmd < ./config/vscode_extensions | while read -r extension || [[ -n $extension ]];
+# do
+#   "codium --install-extension $extension --force"
+# done
