ea3910fc4578638f09da35314fd4d5bafbd62f97
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2025-08-19 20:14:19 -0700

    feat: update setup_macos.sh

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -24,73 +24,10 @@ printf '\n<<< Starting macOS Setup >>>\n'
 osascript -e 'tell application "System Preferences" to quit'
 
 #
-# Values pulled from https://macos-defaults.com
+# Pretty much all of these files/values were pulled from the following sources:
+# https://macos-defaults.com
+# https://github.com/mathiasbynens/dotfiles
 #
-
-# Activity Monitor > Update Frequency
-defaults write com.apple.ActivityMonitor "UpdatePeriod" -int "1"
-
-# Desktop > Keep Folders on Top
-defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
-
-# Dock > Icon Size
-defaults write com.apple.dock "tilesize" -int "40"
-
-# Dock > Minimize Animation Effect
-defaults write com.apple.dock "mineffect" -string "suck"
-
-# Dock > Show Recents
-defaults write com.apple.dock "show-recents" -bool "false"
-
-# Finder > Changing File Extension Warning
-defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
-
-# Finder > Empty Trash After 30 Days
-defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true" &&
-
-# Finder > Keep Folders on Top
-defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
-
-# Finder > Save to Documents by Default
-defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false"
-
-# Finder > Show Extensions
-defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
-
-# Finder > Show Path Bar
-defaults write com.apple.finder "ShowPathbar" -bool "true"
-
-# Finder > Quit
-defaults write com.apple.finder "QuitMenuItem" -bool "true"
-
-# Mission Control > Rearrange Automatically
-defaults write com.apple.dock "mru-spaces" -bool "false"
-
-# Safari > Show Full URL
-defaults write com.apple.safari "ShowFullURLInSmartSearchField" -bool "true"
-
-# Screenshots > Set Location
-defaults write com.apple.screencapture "location" -string "$HOME/Pictures/screenshots"
-
-# Time Machine > Offer New Disks for Time Machine Backup
-defaults write com.apple.TimeMachine "DoNotOfferNewDisksForBackup" -bool "true" 
-
-#
-# Extras from https://github.com/mathiasbynens/dotfiles
-#
-
-##########
-# Energy #
-##########
-
-# Sleep the display after 10 minutes
-sudo pmset -a displaysleep 10
-
-# Disable machine sleep while charging
-sudo pmset -c sleep 0
-
-# Set machine sleep to 15 minutes on battery
-sudo pmset -b sleep 15
 
 ##########
 # Screen #
@@ -100,9 +37,35 @@ sudo pmset -b sleep 15
 # Reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17#issuecomment-266633501
 defaults write NSGlobalDomain AppleFontSmoothing -int 1
 
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to the $HOME/Pictures/screenshots
+defaults write com.apple.screencapture "location" -string "$HOME/Pictures/screenshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Enable HiDPI display modes (requires restart)
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
 ##########
 # Finder #
 ##########
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 # Set Desktop as the default location for new Finder windows
 # For other paths, use `PfLo` and `file:///full/path/here/`
@@ -116,8 +79,52 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-# Speed up Mission Control animations
-defaults write com.apple.dock expose-animation-duration -float 0.1
+# Changing File Extension Warning
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+
+# Empty Trash After 30 Days
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true" &&
+
+# Keep Folders on Top
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+
+# Keep Folders on Top on Desktop
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
+
+# Save to Documents by Default
+defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "true"
+
+# Show Extensions
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+
+# Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Show Path Bar
+defaults write com.apple.finder "ShowPathbar" -bool "true"
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Quit
+defaults write com.apple.finder "QuitMenuItem" -bool "true"
+
+# Disable window animations and Get Info animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
 
 ##########
 # Safari #
@@ -162,12 +169,28 @@ defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
 # Update extensions automatically
 defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 
+# Show Full URL
+defaults write com.apple.safari "ShowFullURLInSmartSearchField" -bool "true"
+
+# Update extensions automatically
+defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
+
 ########
 # Mail #
 ########
 
 # Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
 defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Disable inline attachments (just show the icons)
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+# Disable automatic spell checking
+defaults write com.apple.mail SpellCheckingBehavior -string "NoSpellCheckingEnabled"
+
+# Disable send and reply animations in Mail.app
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
 
 ############
 # Terminal #
@@ -176,12 +199,169 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
+###########
+# General #
+###########
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+########################################
+# Keyboard, Touchpad, Mouse, Bluetooth #
+########################################
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+#################
+# Energy saving #
+#################
+
+# Enable lid wakeup
+sudo pmset -a lidwake 1
+
+# Restart automatically on power loss
+sudo pmset -a autorestart 1
+
+# Restart automatically if the computer freezes
+sudo systemsetup -setrestartfreeze on
+
+# Sleep the display after 10 minutes
+sudo pmset -a displaysleep 10
+
+# Disable machine sleep while charging
+sudo pmset -c sleep 0
+
+# Set machine sleep to 5 minutes on battery
+sudo pmset -b sleep 15
+
+# Set standby delay to 24 hours (default is 1 hour)
+sudo pmset -a standbydelay 86400
+
+# Never go into computer sleep mode
+sudo systemsetup -setcomputersleep Off > /dev/null
+
+# Hibernation mode
+# 0: Disable hibernation (speeds up entering sleep mode)
+# 3: Copy RAM to disk so the system state can still be restored in case of a
+#    power failure.
+sudo pmset -a hibernatemode 0
+
+# Remove the sleep image file to save disk space
+sudo rm /private/var/vm/sleepimage
+# Create a zero-byte file instead…
+sudo touch /private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /private/var/vm/sleepimage
+
+########################################
+# Dock, Dashboard, and Mission Control #
+########################################
+
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Enable spring loading for all Dock items
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Mission Control > Rearrange Automatically
+defaults write com.apple.dock "mru-spaces" -bool "false"
+
+###############################################################################
+# Activity Monitor                                                            #
+###############################################################################
+
+# Show the main window when launching Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+# Activity Monitor > Update Frequency
+defaults write com.apple.ActivityMonitor "UpdatePeriod" -int "2"
+
 ########
 # Misc #
 ########
 
 # Prevent Photos from opening automatically when devices are plugged in
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+# Time Machine > Offer New Disks for Time Machine Backup
+defaults write com.apple.TimeMachine "DoNotOfferNewDisksForBackup" -bool "true" 
+
+
+#######
+# End #
+#######
 
 killall Finder
 killall Dock
